a3c31f64e5a14de1e5dea81b1eb78b598681b1a9
louisoduor <louisodhiambo20@gmail.com>
2024-03-22 22:19:53 +0000

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -10,18 +10,16 @@ Department.drop_table()
 Department.create_table()
 
 payroll = Department.create("Payroll", "Building A, 5th Floor")
-print(payroll) 
-
+print(payroll)  
 hr = Department.create("Human Resources", "Building C, East Wing")
 print(hr) 
-
 hr.name = 'HR'
 hr.location = "Building F, 10th Floor"
 hr.update()
-print(hr)  
+print(hr) 
 
 print("Delete Payroll")
 payroll.delete()  
-print(payroll)  
+print(payroll) 
 
 ipdb.set_trace()
